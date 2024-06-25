d5d2d53cd48686838ec38d72cd649c876f26c779
MEUNIER Sébastien <stg3005@brufor.lan>
2024-06-25 13:10:38 +0200

    first commit

---
+++ Assets/#Project/Scripts/PortalCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class PortalCamera : MonoBehaviour
+{
+    [SerializeField] Camera portalCamera;
+    Camera mainCamera;
+    [SerializeField] Transform portal1, portal2;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 localPosition = portal1.InverseTransformPoint(transform.position);
+        localPosition = Quaternion.AngleAxis(180, Vector3.up) * localPosition;
+        portalCamera.transform.position = portal2.TransformPoint(localPosition);
+
+        Quaternion localRotation = Quaternion.Inverse(portal1.rotation) * transform.rotation;
+        localRotation = Quaternion.Euler(0, 180, 0) * localRotation;
+        portalCamera.transform.rotation = portal2.rotation * localRotation;
+
+        Plane p = new Plane(-portal2.forward, portal2.position);
+        Vector4 planeWorldSpace = new Vector4(p.normal.x, p.normal.y, p.normal.z, p.distance);
+        //portalCamera.worldToCameraMatrix
+    }
+}
